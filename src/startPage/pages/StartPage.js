938711798bb2433edadd2086ced34a618c8d9a7b
mingboi95 <54308224+mingboi95@users.noreply.github.com>
2020-02-18 11:23:31 +0800

    Update StartPage.js

--- src/startPage/pages/StartPage.js
+++ src/startPage/pages/StartPage.js
@@ -26,6 +26,7 @@ function StartPage() {
   };
   // End progress bar context
 
+<<<<<<< Updated upstream
   // Form for property damage and personal injury
   const [committingFormState, committingFormInputHandler] = useForm(
     {
@@ -33,6 +34,72 @@ function StartPage() {
       dateOfIncident: {
         value: "",
         isValid: false
+=======
+  // General Form Type
+  // inputs -> {input -> value, type, isValid}
+  // after the first selector, narrow the form type. the switch statement
+  // should by dynamic
+
+  const formStorage = {
+    "Commencing An Action": {
+      "Property Damage": {
+        dateOfIncident: {
+          value: "",
+          validators: [VALIDATOR_REQUIRE(), VALIDATOR_DATE()],
+          type: "input",
+          output:
+            "If the incident is within 6 years, you are still within the time limit to sue. Unfortunately, if it has been more than 6 years, the time limit for commencing an action for property damage has passed.",
+          placeholder: "DD/MM/YYYY",
+          label: "Date",
+          errorText: "Please enter a valid date and format (DD/MM/YYYY).",
+          isValid: false
+        },
+        valueOfClaim: {
+          value: 0,
+          validators: [VALIDATOR_REQUIRE(), VALIDATOR_NUMBER()],
+          type: "input",
+          output:
+            "Information on how to calculate value of claim for personal injury",
+          placeholder: "Value of claim",
+          label: "Value of claim",
+          errorText: "Please enter a valid value",
+          isValid: false
+        },
+        end: {
+          type: "output",
+          output: "",
+          endFunction: (dateOfIncident, valueOfClaim) => {
+            // If dateOfIncident within 6 years, can sue - show procedures to suing
+            // Process dependent on value you are suing for
+            let output = "";
+            const sixYearAgo = moment().subtract(6, "years").format("DD/MM/YYYY");
+            const sixYearsHavePassed = moment(sixYearAgo).isAfter(dateOfIncident);
+            console.log(sixYearAgo);
+            console.log(sixYearsHavePassed);
+            if (sixYearsHavePassed) {
+              output = "Unfortunately, the time limit for commencing an action for property damage has passed. "
+            } else {
+              const x = parseInt(valueOfClaim);
+              switch (true) {
+                case (x < 20000):
+                  output = output + "Your Claim needs to be filed in the Small Claims Tribunal.";
+                  break;
+                case (x < 30000):
+                  output = output + "If you and the Respondent both agree, this claim can be filed in the Small Claims Tribunal. Otherwise, then it needs to be filed in the Magistrate's Court."
+                  break;
+                case (x < 60000):
+                  output = output + "Your claim needs to be filed in the Magistrate's Court.";
+                  break;
+                default:
+                  output = output + "Your claim needs to be filed in either the District Court or the Hight Court based on the value of your claim. You ought to consult a lawyer for advice."
+              }
+
+              return output;
+            }
+
+          }
+        }
+>>>>>>> Stashed changes
       },
       valueOfClaim: {
         value: 0,
