0ea70fc800167b4f0ea22a8a8adf40911fb8dfdd
mingboi95 <54308224+mingboi95@users.noreply.github.com>
2020-02-20 10:40:12 +0800

    Updated Resources.js
    Cleaned up some elements

--- src/resources/pages/Resources.js
+++ src/resources/pages/Resources.js
@@ -75,16 +75,20 @@ const useStyles = makeStyles(theme => ({
 
 const tiers = [
     {
-        title: 'Free',
-        price: '0',
-        description: ['10 users included', '2 GB of storage', 'Help center access', 'Email support'],
-        buttonText: 'Sign up for free',
+        title: 'Small Claims',
+        price: '20,000',
+        description: ['Claims not exceeding $20,000 ',
+            'Time limit of 2 years',
+            'Help center access',
+            'Email support'],
+        buttonText: 'Small Claims Court',
         buttonVariant: 'outlined',
+        buttonLink: 'https://www.statecourts.gov.sg/CJTS/#!/index1'
     },
     {
-        title: 'Pro',
-        subheader: 'Most popular',
-        price: '15',
+        title: 'District Court',
+        subheader: 'Most Popular',
+        price: '250,000',
         description: [
             '20 users included',
             '10 GB of storage',
@@ -93,18 +97,24 @@ const tiers = [
         ],
         buttonText: 'Get started',
         buttonVariant: 'contained',
+        buttonLink: ''
     },
     {
-        title: 'Enterprise',
-        price: '30',
+        title: 'Magistrate Court',
+        price: '60,000',
         description: [
-            '50 users included',
-            '30 GB of storage',
-            'Help center access',
-            'Phone & email support',
+            'Application for Document Discovery',
+            'Amendment for filed documents',
+            'Application for default judgement',
+            'Application for summary judgement',
+            'Application for interlocutory injuction',
+            'Application for Mareva injuction',
+            'Application for Anton Piller Order',
+            'Application for Security for costs'
         ],
         buttonText: 'Contact us',
         buttonVariant: 'outlined',
+        buttonLink: ''
     },
 ];
 const footers = [
@@ -132,60 +142,39 @@ export default function Pricing() {
     return (
         <React.Fragment>
             <CssBaseline />
-            <AppBar position="static" color="default" elevation={0} className={classes.appBar}>
-                <Toolbar className={classes.toolbar}>
-                    <Typography variant="h6" color="inherit" noWrap className={classes.toolbarTitle}>
-                        Company name
-          </Typography>
-                    <nav>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Features
-            </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Enterprise
-            </Link>
-                        <Link variant="button" color="textPrimary" href="#" className={classes.link}>
-                            Support
-            </Link>
-                    </nav>
-                    <Button href="#" color="primary" variant="outlined" className={classes.link}>
-                        Login
-          </Button>
-                </Toolbar>
-            </AppBar>
             {/* Hero unit */}
             <Container maxWidth="sm" component="main" className={classes.heroContent}>
                 <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-                    Pricing
+                    Resources
         </Typography>
                 <Typography variant="h5" align="center" color="textSecondary" component="p">
-                    Quickly build an effective pricing table for your potential customers with this layout.
-                    It&apos;s built with default Material-UI components with little customization.
+                    Insert description here
         </Typography>
             </Container>
             {/* End hero unit */}
-            <Container maxWidth="md" component="main">
-                <Grid container spacing={5} alignItems="flex-end">
+            <Container maxWidth="lg" component="main">
+                <Grid container spacing={5} alignItems="flex-start">
                     {tiers.map(tier => (
                         // Enterprise card is full width at sm breakpoint
-                        <Grid item key={tier.title} xs={12} sm={tier.title === 'Enterprise' ? 12 : 6} md={4}>
+                        <Grid item key={tier.title} xs={3} sm={tier.title === 'Magistrate Court' ? 12 : 4} md={4}>
                             <Card>
                                 <CardHeader
                                     title={tier.title}
                                     subheader={tier.subheader}
                                     titleTypographyProps={{ align: 'center' }}
                                     subheaderTypographyProps={{ align: 'center' }}
-                                    action={tier.title === 'Pro' ? <StarIcon /> : null}
+                                    //action={tier.title === 'District Court' ? <StarIcon /> : null}
                                     className={classes.cardHeader}
                                 />
                                 <CardContent>
                                     <div className={classes.cardPricing}>
-                                        <Typography component="h2" variant="h3" color="textPrimary">
+                                        <Typography component="h3" variant="h3" color="textPrimary">
                                             ${tier.price}
                                         </Typography>
                                         <Typography variant="h6" color="textSecondary">
-                                            /mo
+                                            or below
                     </Typography>
+
                                     </div>
                                     <ul>
                                         {tier.description.map(line => (
